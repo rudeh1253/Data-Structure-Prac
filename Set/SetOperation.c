e4bb77421e24cff9580ac7f79c941f2af6ce2628
PGD <rudeh1253@gmail.com>
2021-12-20 23:03:50 +0900

    started to implement setIntersect

--- Set/SetOperation.c
+++ Set/SetOperation.c
@@ -7,6 +7,7 @@
 int count = 0;
 
 void setUnionImpl(Set* sUnion, Set* setA, Set* setB);
+void setIntersectImpl(Set* sIntersect, Set* setA, Set* setB);
 
 Set setUnion(Set* setA, Set* setB) {
     Set sUnion;
@@ -30,12 +31,12 @@ void setUnionImpl(Set* sUnion, Set* setA, Set* setB) {
 
     if (setA->list->cur->next == setA->list->trailer) {
         if (setB->list->cur->next != setB->list->trailer) {
-            printf("if1 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
+            //printf("if1 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
             setAdd(sUnion, listGet(setB->list));
             listToNext(setB->list);
             setUnionImpl(sUnion, setA, setB);
         } else {
-            printf("if2 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
+            //printf("if2 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
             setAdd(sUnion, listGet(setA->list));
             setAdd(sUnion, listGet(setB->list));
 
@@ -43,12 +44,12 @@ void setUnionImpl(Set* sUnion, Set* setA, Set* setB) {
         }
     } else if (setB->list->cur->next == setB->list->trailer) {
         if (setA->list->cur->next != setA->list->trailer) {
-            printf("if3 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
+            //printf("if3 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
             setAdd(sUnion, listGet(setA->list));
             listToNext(setA->list);
             setUnionImpl(sUnion, setA, setB);
         } else {
-            printf("if4 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
+            //printf("if4 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
             setAdd(sUnion, listGet(setA->list));
             setAdd(sUnion, listGet(setB->list));
 
@@ -56,18 +57,18 @@ void setUnionImpl(Set* sUnion, Set* setA, Set* setB) {
         }
     } else {
         if (setA->list->cur == setA->list->header || setB->list->cur == setB->list->header) {
-            printf("if5 - %d\n", ++count);
+            //printf("if5 - %d\n", ++count);
             setA->list->cur = setA->list->header->next;
             setB->list->cur = setB->list->header->next;
         }
 
         if (setA->list->comparator(listGet(setA->list), listGet(setB->list))) {
-            printf("if6 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
+            //printf("if6 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
             setAdd(sUnion, listGet(setA->list));
             listToNext(setA->list);
             setUnionImpl(sUnion, setA, setB);
         } else {
-            printf("if7 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
+            //printf("if7 - %d - %d, %d\n", ++count, *(int*)listGet(setA->list), *(int*)listGet(setB->list));
             setAdd(sUnion, listGet(setB->list));
             listToNext(setB->list);
             setUnionImpl(sUnion, setA, setB);
@@ -76,7 +77,19 @@ void setUnionImpl(Set* sUnion, Set* setA, Set* setB) {
 }
 
 Set setIntersect(Set* setA, Set* setB) {
-     
+    Set sIntersect;
+    setInit(&sIntersect);
+
+    setIntersectImpl(sIntersect, setA, setB);
+
+    return setIntersect;
+}
+
+void setIntersectImpl(Set* sIntersect, Set* setA, Set* setB) {
+    if (setA->list->cur->next == setA->list->trailer && setB->list->cur->next == setB->list->trailer) {
+
+        return;
+    }
 }
 
 Set setSubtract(Set* setA, Set* setB) {
